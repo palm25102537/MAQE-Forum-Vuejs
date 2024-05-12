53061d7842bb2df4595cac5cb88c75286da85b2b
Wasin <wasin.ho@logicspark.com>
2024-05-12 18:42:16 +0700

    add error description

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-full h-full flex items-center justify-center min-h-device">
-    <h1 class="text-[48px] text-red-500 font-bold">Error Page</h1>
+  <div
+    class="w-full h-full flex flex-col items-center justify-center min-h-device"
+  >
+    <h1 class="text-[48px] text-red-500 font-bold">Error</h1>
+    <p class="text-[24px]">Something went wrong please try again later</p>
   </div>
 </template>
